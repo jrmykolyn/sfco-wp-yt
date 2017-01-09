35a0d9f5f929a5c0761dba16e6f85fd5665dcf08
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-08 23:25:25 -0500

    Built out markup and styles for 'post byline'.

--- includes/post/_post-header.php
+++ includes/post/_post-header.php
@@ -2,4 +2,11 @@
     <a href="#" class="post-tag--leading">Post Tag</a>
     <h1><?= the_title(); ?></h1>
     <p class="post-copy--leading"><?= get_field( 'dek' ); ?></p>
+    <div class="post-byline">
+        <img src="https://placeholdit.imgix.net/~text?txtsize=12&txt=IMG&w=50&h=50" alt="#">
+        <p>
+            <span>Author Name</span>
+            <span>Publication Date</span>
+        </p>
+    </div><!-- /.post-byline -->
 </div><!-- /.post-header -->
