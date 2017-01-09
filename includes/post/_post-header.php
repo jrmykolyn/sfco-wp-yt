938ba89cfa7dfbcc3cf34f582b5eb6f2b000f6ad
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-08 23:28:08 -0500

    Replaced hardcoded 'post byline' content with calls to WP functions.

--- includes/post/_post-header.php
+++ includes/post/_post-header.php
@@ -5,8 +5,8 @@
     <div class="post-byline">
         <img src="https://placeholdit.imgix.net/~text?txtsize=12&txt=IMG&w=50&h=50" alt="#">
         <p>
-            <span>Author Name</span>
-            <span>Publication Date</span>
+            <span><?= get_the_author(); ?></span>
+            <span><?= get_the_date(); ?></span>
         </p>
     </div><!-- /.post-byline -->
 </div><!-- /.post-header -->
