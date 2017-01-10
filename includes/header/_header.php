4700f858faaf8269ecf5bc15c58b17f5c8deae9d
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-10 01:31:08 -0500

    Added link to 'home'/index page in 'header' component.

--- includes/header/_header.php
+++ includes/header/_header.php
@@ -1,8 +1,8 @@
 <header class="header">
     <div class="header__inner">
-        <div class="logo-wrap">
+        <a href="<?= get_site_url(); ?>"class="logo-wrap">
             <img src="http://placeholdit.imgix.net/~text?txtsize=24&txt=LOGO&w=150&h=50" alt="#" />
-        </div>
+        </a>
         <nav class="nav">
         <?php wp_nav_menu(); ?>
         </nav><!-- /.nav -->
