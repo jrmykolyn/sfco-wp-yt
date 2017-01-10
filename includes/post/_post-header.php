d09058e620e3a87172190a1f0f8a5d058f1ce38c
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-10 01:01:00 -0500

    Updated post 'byline' styles.

--- includes/post/_post-header.php
+++ includes/post/_post-header.php
@@ -2,9 +2,9 @@
     <a href="#" class="post-tag--leading">Post Tag</a>
     <h1><?= the_title(); ?></h1>
     <p class="post-copy--leading"><?= get_field( 'dek' ); ?></p>
-    <div class="post-byline">
+    <div class="post-byline-wrap">
         <img src="https://placeholdit.imgix.net/~text?txtsize=12&txt=IMG&w=50&h=50" alt="#">
-        <p>
+        <p class="post-byline">
             <span><?= get_the_author(); ?></span>
             <span><?= get_the_date(); ?></span>
         </p>
