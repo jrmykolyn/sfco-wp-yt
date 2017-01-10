602039fdf0d9a1df0010fdc7e2d9abdbe972dd58
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-10 01:28:27 -0500

    Updated markup and styles for theme header; added WP menu markup.

--- includes/header/_header.php
+++ includes/header/_header.php
@@ -1,7 +1,10 @@
 <header class="header">
     <div class="header__inner">
+        <div class="logo-wrap">
+            <img src="http://placeholdit.imgix.net/~text?txtsize=24&txt=LOGO&w=150&h=50" alt="#" />
+        </div>
         <nav class="nav">
-            <h2>Header Component</h2>
+        <?php wp_nav_menu(); ?>
         </nav><!-- /.nav -->
     </div><!-- /.header__inner -->
 </header><!-- /.header -->
