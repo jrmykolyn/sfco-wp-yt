5538e8a0f343a74a512230f7f0d6c8460e3b464d
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-11 11:00:01 -0500

    Updated project JS to allow for closing of 'preheader' elem.

--- includes/preheader/_preheader.php
+++ includes/preheader/_preheader.php
@@ -4,5 +4,5 @@
             Magna do Lorem amet duis ipsum qui consectetur elit culpa sit nisi incididunt.
         </p>
     </div>
-    <button type="button" name="button" class="preheader__cta"></button>
+    <button type="button" name="button" class="preheader__cta js--close" data-close-target="preheader"></button>
 </section><!-- /.preheader -->
--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,21 @@ $( document ).ready( function() {
     /* -------------------------------------------------- */
     var body = $( 'body' );
     var toggle = $( '.js--drawer-nav-toggle' );
+    var btn_close = $( '.js--close' );
 
 
     /* -------------------------------------------------- */
     /* EVENTS */
     /* -------------------------------------------------- */
+    if ( btn_close ) {
+        btn_close.on( 'click', function( event ) {
+            var target_class = $( this ).data( 'close-target' );
+            var target_elem = $( this ).closest( '.' + target_class );
+
+            target_elem.slideUp( 200 );;
+        } );
+    }
+
     if ( toggle ) {
         toggle.on( 'click', function( event ) {
             body.toggleClass( 'drawer-nav-active' );
