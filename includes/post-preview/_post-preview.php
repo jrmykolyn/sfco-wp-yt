34e4ec7ab57e77f7ae6cc448ce3b5fb4ef81bd7f
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-11 00:34:04 -0500

    Updated 'post-preview' and related styles.

--- includes/post-preview/_post-preview.php
+++ includes/post-preview/_post-preview.php
@@ -1,23 +1,23 @@
 <article class="post-preview">
-    <div class="post-preview__header">
-        <a href="#" class="post-tag--preview">Test Tag 123</a>
-    </div>
-    <div class="post-preview__body">
-        <a href="<?= get_permalink(); ?>">
+    <a href="<?= get_permalink(); ?>"class="post-preview__inner">
+        <div class="post-preview-header">
+            <span class="post-tag--preview">Test Tag 123</span>
+        </div>
+        <div class="post-preview-body">
             <h2><?= the_title(); ?></h2>
-        </a>
-        <p>
-        <?php
-        if ( get_field( 'dek' ) ):
-            echo get_field( 'dek' );
-        endif;
-        ?>
-        </p>
-    </div>
-    <div class="post-preview__footer">
-        <p class="post-byline">
-            <span><?= get_the_author(); ?></span>
-            <span><?= get_the_date(); ?></span>
-        </p>
-    </div>
+            <p>
+            <?php
+            if ( get_field( 'dek' ) ):
+                echo get_field( 'dek' );
+            endif;
+            ?>
+            </p>
+        </div>
+        <div class="post-preview-footer">
+            <p class="post-byline">
+                <span><?= get_the_author(); ?></span>
+                <span><?= get_the_date(); ?></span>
+            </p>
+        </div>
+    </a><!-- /.post-preview__inner -->
 </article><!-- /.post-preview -->
