41bb1b3d1b17c90920e057dd3c224e1f55c6cc29
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-11 01:08:13 -0500

    Split existing 'nav' styles off into dedicated partial file; updated 'header' styles.

--- includes/header/_header.php
+++ includes/header/_header.php
@@ -1,10 +1,18 @@
 <header class="header">
     <div class="header__inner">
-        <a href="<?= get_site_url(); ?>"class="logo-wrap">
-            <img src="http://placeholdit.imgix.net/~text?txtsize=24&txt=LOGO&w=150&h=50" alt="#" />
-        </a>
+        <div class="logo-wrap">
+            <a href="<?= get_site_url(); ?>" class="logo-wrap__link">
+                <img src="http://placeholdit.imgix.net/~text?txtsize=24&txt=LOGO&w=150&h=50" alt="#" />
+            </a><!-- /.logo-wrap__link -->
+        </div><!-- /.logo-wrap -->
+        
         <nav class="nav">
-        <?php wp_nav_menu(); ?>
+        <?php wp_nav_menu(
+            array(
+                'link_before' => '<span class="nav-link-text">',
+                'link_after' => '</span>',
+            )
+        ); ?>
         </nav><!-- /.nav -->
     </div><!-- /.header__inner -->
 </header><!-- /.header -->
