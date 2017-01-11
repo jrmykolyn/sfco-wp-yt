8294ea76007962304f8fc28382803a91629a371f
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-11 02:03:26 -0500

    Moved theme JS ref. out of partial file: now added via 'enqueue'.

--- functions.php
+++ functions.php
@@ -1,3 +1,8 @@
 <?php
+/* THEME DEFAULTS */
 add_theme_support( 'post-thumbnails' );
+
+/* THEME SCRIPTS */
+wp_register_script( 'main', get_stylesheet_directory_uri() . '/js/main.min.js', '', '', true );
+wp_enqueue_script( 'main' );
 ?>
