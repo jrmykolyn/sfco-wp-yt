2843164b83bf501ac728799e57b0714e499fccb1
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-15 21:32:16 -0500

    Updated 'single' template to only display 'hero' component if current post has a 'featured image'.

--- single.php
+++ single.php
@@ -1,7 +1,11 @@
 <?php
+$thumb = get_the_post_thumbnail();
+
 get_header();
 while ( have_posts() ): the_post();
-    get_template_part( 'includes/post/_post-hero' );
+    if ( $thumb ):
+        get_template_part( 'includes/post/_post-hero' );
+    endif;
 ?>
 <main class="main post"><!-- /// TEMP - Move `post` class to new element; rebuild template. -->
     <section class="layout-section">
