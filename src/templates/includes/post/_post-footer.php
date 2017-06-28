be062233b87902cca6a3f68971d3705861e040da
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-27 23:28:23 -0400

    Updated 'post footer' partial to correctly print out tags for curr. post.

--- src/templates/includes/post/_post-footer.php
+++ src/templates/includes/post/_post-footer.php
@@ -1,6 +1,7 @@
 <div class="post-footer">
 <?php
-$tags = get_tags();
+$tags = wp_get_post_tags( get_the_ID() );
+
 foreach( $tags as $tag ):
 ?>
     <a href="<?= get_tag_link( $tag->term_id ); ?>" class="post-tag"><?= $tag->name; ?></a>
