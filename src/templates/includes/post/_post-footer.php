85fd6f1474d474bcf16634994f9f22074bf1f287
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-28 19:38:41 -0400

    Updated structure of 'post footer' partial, added 'tags' logic. Split 'post footer' SASS partial off from 'post'.

--- src/templates/includes/post/_post-footer.php
+++ src/templates/includes/post/_post-footer.php
@@ -1,11 +1,17 @@
 <div class="post-footer">
-<?php
-$tags = wp_get_post_tags( get_the_ID() );
+    <!-- FOOTER TAGS -->
+    <?php
+    $tags = wp_get_post_tags( get_the_ID() );
 
-foreach( $tags as $tag ):
-?>
-    <a href="<?= get_tag_link( $tag->term_id ); ?>" class="post-tag"><?= $tag->name; ?></a>
-<?php
-endforeach;
-?>
+    if ( isset( $tags ) && count( $tags ) ):
+    ?>
+    <div class="post-footer__tags">
+        <h2><?= __( 'Tags' ); ?></h2>
+    <?php foreach( $tags as $tag ): ?>
+        <a href="<?= get_tag_link( $tag->term_id ); ?>" class="post-tag"><?= $tag->name; ?></a>
+    <?php endforeach; ?>
+    </div><!-- /.post-footer__tags -->
+    <?php
+    endif;
+    ?>
 </div><!-- /.post-footer-->
