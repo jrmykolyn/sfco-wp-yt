06f70c224b905feed5c8023ebe4a64f24ab0552f
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-28 19:21:51 -0400

    Created '_pagination.php' partial file.

---
+++ src/templates/includes/pagination/_pagination.php
@@ -0,0 +1,9 @@
+<div class="pagination-wrapper">
+<?php
+// Previous/next page navigation.
+the_posts_pagination( array(
+    'prev_text' => __( 'Previous', 'sfco-wp-yt' ),
+    'next_text' => __( 'Next', 'sfco-wp-yt' )
+) );
+?>
+</div>
