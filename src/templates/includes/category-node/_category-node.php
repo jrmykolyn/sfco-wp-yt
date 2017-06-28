0258342768d838916e384c6563b4e1573a7a3da8
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-28 19:31:55 -0400

    Built out 'category node' PHP partial ('_category-node.php').

---
+++ src/templates/includes/category-node/_category-node.php
@@ -0,0 +1,9 @@
+<?php
+$category = get_first_post_category( get_the_ID() );
+
+if ( $category ):
+?>
+    <a href="<?= get_category_link( $category->cat_ID ); ?>" class="category-node"><?= $category->name; ?></a>
+<?php
+endif;
+?>
