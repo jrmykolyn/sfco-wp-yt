63a21934453d2921eee1404255769a7168eb0c67
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-02 22:10:16 -0500

    Expose 'secondary' and 'socials' menus.

--- src/templates/functions.php
+++ src/templates/functions.php
@@ -27,9 +27,11 @@ add_action( 'init', 'register_menus' );
  */
 function register_menus() {
    register_nav_menus(
-	   array(
-		   'primary' => __( 'Primary' )
-	   )
+        array(
+           'primary' => __( 'Primary' ),
+           'secondary' => __( 'Secondary' ),
+           'socials' => __( 'Socials '),
+        )
    );
 }
 
