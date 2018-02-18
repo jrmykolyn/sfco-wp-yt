b6456581b33e0916ea0e42886713c30c9665709f
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-02-18 16:28:36 -0500

    Increase width of main content area in 'single' template. Increase size of type at 960px and above.

--- src/templates/single.php
+++ src/templates/single.php
@@ -9,14 +9,14 @@ while ( have_posts() ): the_post();
 ?>
 <main class="main post"><!-- /// TEMP - Move `post` class to new element; rebuild template. -->
     <section class="layout-section">
-        <div class="layout-section__inner--narrow">
+        <div class="layout-section__inner">
         <?php
         get_template_part( 'includes/post/_post-header' );
         get_template_part( 'includes/post/_post-body' );
         get_template_part( 'includes/post/_post-footer' );
         ?>
-        </div><!-- /.layout-section__inner--narrow -->
-    </section><!-- /.layout-section -->
+        </div>
+    </section>
 </main>
 
 <?php
