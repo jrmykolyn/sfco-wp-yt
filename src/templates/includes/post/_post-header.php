3bdcb8e7a7b9ff10140362e0c633fc4effa94d34
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-02-18 16:42:30 -0500

    Remove placeholder 'Author' image in 'single' template, update styles. Remove 'byline' rules/dividers.

--- src/templates/includes/post/_post-header.php
+++ src/templates/includes/post/_post-header.php
@@ -3,7 +3,6 @@
     <h1><?= the_title(); ?></h1>
     <p class="post-copy--leading"><?= get_field( 'dek' ); ?></p>
     <div class="post-byline-wrap">
-        <img src="https://placeholdit.imgix.net/~text?txtsize=12&txt=IMG&w=50&h=50" alt="#">
         <p class="post-byline">
             <span><?= get_the_author(); ?></span>
             <span><?= get_the_date(); ?></span>
