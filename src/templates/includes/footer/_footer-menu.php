2a87a5a0c39d417fd80307cabb1a9d0bd16ec0a7
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-02-21 08:52:46 -0500

    Only display footer menu markup if menu actually exists at specified location.

--- src/templates/includes/footer/_footer-menu.php
+++ src/templates/includes/footer/_footer-menu.php
@@ -1,9 +1,15 @@
-<div class="footer-col">
 <?php
-wp_nav_menu( array(
-    'theme_location' => $menu_location,
-    'container' => false,
-    'menu_class' => 'footer-menu',
-) );
+if ( has_nav_menu( $menu_location ) ) {
 ?>
-</div>
+    <div class="footer-col">
+    <?php
+    wp_nav_menu( array(
+        'theme_location' => $menu_location,
+        'container' => false,
+        'menu_class' => 'footer-menu',
+    ) );
+    ?>
+    </div>
+<?php
+}
+?>
