66997a07fd298efc5566130343db4441dbb54a03
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-02-21 08:32:14 -0500

    Add placeholder 'logo' asset to theme, update template. Fix formatting in 'header.php'.

--- src/templates/includes/header/_header.php
+++ src/templates/includes/header/_header.php
@@ -2,23 +2,23 @@
     <div class="header__inner">
         <div class="logo-wrap">
             <a href="<?= get_site_url(); ?>" class="logo-wrap__link">
-                <img src="http://placeholdit.imgix.net/~text?txtsize=24&txt=LOGO&w=150&h=50" alt="#" />
+                <img src="<?= get_template_directory_uri() . '/img/placeholder/logo.png' ?>" alt="" />
             </a><!-- /.logo-wrap__link -->
         </div><!-- /.logo-wrap -->
 
         <nav class="nav">
-		<?php
-		if ( has_nav_menu( 'primary' ) ) {
-			wp_nav_menu(
-				array(
-					'theme_location' => 'primary',
-					'menu_class' => 'header-menu',
-					'link_before' => '<span class="nav-link-text">',
-					'link_after' => '</span>'
-				)
-			);
-		}
-		?>
+            <?php
+            if ( has_nav_menu( 'primary' ) ) {
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'primary',
+                        'menu_class' => 'header-menu',
+                        'link_before' => '<span class="nav-link-text">',
+                        'link_after' => '</span>'
+                    )
+                );
+            }
+            ?>
         </nav><!-- /.nav -->
         <button type="button" name="drawer-nav-toggle" class="drawer-nav-toggle js--drawer-nav-toggle">
             <span class="drawer-nav-toggle__elem"></span>
