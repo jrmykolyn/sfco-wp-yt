2e9a6e7363c68ad0e65b8732416de66a42a17812
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-02-23 08:29:17 -0500

    Fix formatting in JS files.

--- src/js/main.js
+++ src/js/main.js
@@ -1,68 +1,68 @@
 $( document ).ready( function() {
-    /* -------------------------------------------------- */
-    /* DECLARE VARS */
-    /* -------------------------------------------------- */
-    var MODIFIER_CLASSES = {
-        'fade-in': '.fade-in',
-        'faded-in': '.faded-in'
-    };
+	/* -------------------------------------------------- */
+	/* DECLARE VARS */
+	/* -------------------------------------------------- */
+	var MODIFIER_CLASSES = {
+		'fade-in': '.fade-in',
+		'faded-in': '.faded-in'
+	};
 
-    var body = $( 'body' );
-    var toggle = $( '.js--drawer-nav-toggle' );
-    var drawerNavElem = $( '.js--drawer-nav' );
-    var btn_close = $( '.js--close' );
-    var fade_in = $ (MODIFIER_CLASSES[ 'fade-in' ] );
-
-
-    /* -------------------------------------------------- */
-    /* DECLARE FUNCTIONS */
-    /* -------------------------------------------------- */
-     function classToString( className ) {
-         if ( className.substr( 0, 1 ) === '.' ) {
-             return className.substr( 1 );
-         }
-
-          return className;
-     }
+	var body = $( 'body' );
+	var toggle = $( '.js--drawer-nav-toggle' );
+	var drawerNavElem = $( '.js--drawer-nav' );
+	var btn_close = $( '.js--close' );
+	var fade_in = $ (MODIFIER_CLASSES[ 'fade-in' ] );
 
 
-    /* -------------------------------------------------- */
-    /* EVENTS */
-    /* -------------------------------------------------- */
-    if ( btn_close ) {
-        btn_close.on( 'click', function( event ) {
-            var target_class = $( this ).data( 'close-target' );
-            var target_elem = $( this ).closest( '.' + target_class );
+	/* -------------------------------------------------- */
+	/* DECLARE FUNCTIONS */
+	/* -------------------------------------------------- */
+	function classToString( className ) {
+		if ( className.substr( 0, 1 ) === '.' ) {
+			return className.substr( 1 );
+		}
 
-            target_elem.slideUp( 200 );;
-        } );
-    }
+		return className;
+	}
 
-    if ( toggle ) {
-        toggle.on( 'click', function( event ) {
-            // Update classes.
-            body.toggleClass( 'drawer-nav-active' );
 
-            var isActive = body.hasClass( 'drawer-nav-active' );
-            var tabindexVal = isActive ? 0 : -1;
+	/* -------------------------------------------------- */
+	/* EVENTS */
+	/* -------------------------------------------------- */
+	if ( btn_close ) {
+		btn_close.on( 'click', function( event ) {
+			var target_class = $( this ).data( 'close-target' );
+			var target_elem = $( this ).closest( '.' + target_class );
 
-            // Update elem. `tabindex`.
-            drawerNavElem.find( '[tabindex]' ).each( function( i, el ) {
-                $( el ).attr( 'tabindex', tabindexVal );
-            } );
+			target_elem.slideUp( 200 );;
+		} );
+	}
 
-            // Set focus.
-            if ( isActive ) {
-                drawerNavElem.find( toggle ).focus();
-            } else {
-                toggle.eq( 0 ).focus();
-            }
-        } );
-    }
+	if ( toggle ) {
+		toggle.on( 'click', function( event ) {
+			// Update classes.
+			body.toggleClass( 'drawer-nav-active' );
 
-    if ( fade_in ) {
-        fade_in.each( function( i, el ) {
-            $( el ).addClass( classToString( MODIFIER_CLASSES[ 'faded-in' ] ) );
-        } );
-    }
+			var isActive = body.hasClass( 'drawer-nav-active' );
+			var tabindexVal = isActive ? 0 : -1;
+
+			// Update elem. `tabindex`.
+			drawerNavElem.find( '[tabindex]' ).each( function( i, el ) {
+				$( el ).attr( 'tabindex', tabindexVal );
+			} );
+
+			// Set focus.
+			if ( isActive ) {
+				drawerNavElem.find( toggle ).focus();
+			} else {
+				toggle.eq( 0 ).focus();
+			}
+		} );
+	}
+
+	if ( fade_in ) {
+		fade_in.each( function( i, el ) {
+			$( el ).addClass( classToString( MODIFIER_CLASSES[ 'faded-in' ] ) );
+		} );
+	}
 } );
