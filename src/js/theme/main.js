58d27e64d1882b72d725f53360acab6e5d49d9ef
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-03-01 08:54:05 -0500

    topic-29 Add support for auto closing of 'drawer nav'.

--- src/js/theme/main.js
+++ src/js/theme/main.js
@@ -9,11 +9,15 @@ $( document ).ready( function() {
 		'faded-in': '.faded-in'
 	};
 
+	// Elems.
 	var body = $( 'body' );
 	var toggle = $( '.js--drawer-nav-toggle' );
 	var drawerNavElem = $( '.js--drawer-nav' );
 	var btnCloseElem = $( '.js--close' );
 	var fadeInElems = $ ( MODIFIER_CLASSES[ 'fade-in' ] );
+
+	// Data
+	var windowWidthPrev = null;
 
 	/* -------------------------------------------------- */
 	/* DECLARE FUNCTIONS */
@@ -24,6 +28,26 @@ $( document ).ready( function() {
 		}
 
 		return className;
+	}
+
+	function toggleDrawerNav() {
+		// Update classes.
+		body.toggleClass( 'drawer-nav-active' );
+
+		var isActive = body.hasClass( 'drawer-nav-active' );
+		var tabindexVal = isActive ? 0 : -1;
+
+		// Update elem. `tabindex`.
+		drawerNavElem.find( '[tabindex]' ).each( function( i, el ) {
+			$( el ).attr( 'tabindex', tabindexVal );
+		} );
+
+		// Set focus.
+		if ( isActive ) {
+			drawerNavElem.find( toggle ).focus();
+		} else {
+			toggle.eq( 0 ).focus();
+		}
 	}
 
 	/* -------------------------------------------------- */
@@ -39,24 +63,40 @@ $( document ).ready( function() {
 	}
 
 	if ( toggle ) {
-		toggle.on( 'click', function() {
-			// Update classes.
-			body.toggleClass( 'drawer-nav-active' );
+		// Register 'click' handler.
+		toggle.on( 'click', toggleDrawerNav );
 
-			var isActive = body.hasClass( 'drawer-nav-active' );
-			var tabindexVal = isActive ? 0 : -1;
+		// Handle TAB/SHIT + TAB
+		window.addEventListener( 'keydown', function( e ) {
+			if ( body.hasClass( 'drawer-nav-active' ) ) {
+				var elems = drawerNavElem.find( 'button, a' );
 
-			// Update elem. `tabindex`.
-			drawerNavElem.find( '[tabindex]' ).each( function( i, el ) {
-				$( el ).attr( 'tabindex', tabindexVal );
-			} );
-
-			// Set focus.
-			if ( isActive ) {
-				drawerNavElem.find( toggle ).focus();
-			} else {
-				toggle.eq( 0 ).focus();
+				// If the user hit SHIFT + TAB or TAB when on first/last focusable elem:
+				// - Suppress TAB.
+				// - Toggle drawer nav.
+				if ( e.key.toLowerCase() === 'tab' ) {
+					if (
+						e.shiftKey && document.activeElement === elems.get( 0 ) ||
+						!e.shiftKey & document.activeElement === elems.get( -1 )
+					) {
+						e.preventDefault();
+						toggleDrawerNav();
+					}
+				}
 			}
+		} );
+
+		// Handle resize.
+		window.addEventListener( 'resize', function() {
+			if ( window.outerWidth > windowWidthPrev ) {
+				// When we cross the 768px threshold, close the drawer nav. if required.
+				if ( window.outerWidth >= 768 && body.hasClass( 'drawer-nav-active' ) ) {
+					toggleDrawerNav();
+				}
+			}
+
+			// Update window width.
+			windowWidthPrev = window.outerWidth;
 		} );
 	}
 
