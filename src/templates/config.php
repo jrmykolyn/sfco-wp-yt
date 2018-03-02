e484ca3c0ef9d7f81421d54014306e0c903f0725
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-03-02 09:10:46 -0500

    topic-4 Add 'plugins' data to global CONFIG.

--- src/templates/config.php
+++ src/templates/config.php
@@ -3,5 +3,17 @@ $CONFIG = array(
 	'defaults' => array(
 		'max_featured_posts' => 3,
 	),
+	// NOTE:
+	// - Subarray below used to expose plugin-related info to `functions.php`.
+	'plugins' => array(
+		'acf' => array(
+			'name' => 'Advanced Custom Fields',
+			'url' => 'https://en-ca.wordpress.org/plugins/advanced-custom-fields/',
+		),
+		'wp_user_avatar' => array(
+			'name' => 'WP User Avatar',
+			'url' => 'https://en-ca.wordpress.org/plugins/wp-user-avatar/',
+		),
+	),
 );
 ?>
