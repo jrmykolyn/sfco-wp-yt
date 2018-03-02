28c8120c8c5b93ddff74b1271a4a59e1e06998f2
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-03-02 08:55:34 -0500

    topic-4 Display user avatar on 'post'.

--- src/templates/includes/post/_post-header.php
+++ src/templates/includes/post/_post-header.php
@@ -3,6 +3,7 @@
 	<h1><?= the_title(); ?></h1>
 	<p class="post-copy--leading"><?= get_the_excerpt(); ?></p>
 	<div class="post-byline-wrap">
+		<?= get_avatar( $post->post_author ); ?>
 		<p class="post-byline">
 			<span><?= get_the_author(); ?></span>
 			<span><?= get_the_date(); ?></span>
